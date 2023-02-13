3dabae49dbd5be49b102fe63540a10adf85208ed
Alex Solodkiy <alexsolodkiy@mail.ru>
2023-02-13 23:58:16 +0300

    scroll with gsap

---
+++ index.js
@@ -0,0 +1,52 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if(ScrollTrigger.isTouch !==1){
+    ScrollSmoother.create({
+        wrapper:'.wrapper',
+        content:'.content',
+        smooth:1.6,
+        effects:true
+    })
+
+    gsap.fromTo('.header',{opacity:1},{
+        opacity:0,
+        scrollTrigger:{
+            trigger:'.header',
+            start:'center',
+            end:'820',
+            scrub:true
+        },
+      
+    })
+
+    let itemsL = gsap.utils.toArray('.gallery_l .gallery_item')
+
+    itemsL.forEach(item => {
+        gsap.fromTo(item,{x:-50,opacity:0},{
+            opacity:1,
+            x:0,
+            scrollTrigger:{
+                trigger:item,
+                start:'-1000',
+                end:'0',
+                scrub:true
+            },
+          
+        })
+    });
+    let itemsR = gsap.utils.toArray('.gallery_r .gallery_item')
+
+    itemsR.forEach(item => {
+        gsap.fromTo(item,{x:150,opacity:0},{
+            opacity:1,
+            x:0,
+            scrollTrigger:{
+                trigger:item,
+                start:'-1000',
+                end:'0',
+                scrub:true
+            },
+          
+        })
+    });
+}   
